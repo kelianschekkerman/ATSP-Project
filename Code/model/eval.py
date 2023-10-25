6846b662a717638f2ec5eeb2b0a6711a05098aa2
Ali Satvaty <46220670+alistvt@users.noreply.github.com>
2023-10-25 17:53:33 +0330

    Update eval.py

--- Code/model/eval.py
+++ Code/model/eval.py
@@ -1,7 +1,11 @@
 
 def eval_predictions(eval_data_path, predictions, labels, output_dir):
     # do the evaluations
-
     # save the results as json file in the following file
     output_path = output_dir / f"eval_{eval_data_path.stem}.json"
+
+    print()
+    print("start evaluation".upper().center(40, '='))
+    print(f">>> Will save at: {output_dir}")
+    print()
     
