be9cf42dd1bed6aa2f46e157e6f5bbc2bb572a89
alistvt <alistvt@gmail.com>
2023-10-25 21:46:01 +0200

    fix code training

--- Code/model/eval.py
+++ Code/model/eval.py
@@ -1,7 +1,14 @@
 
 def eval_predictions(eval_data_path, predictions, labels, output_dir):
     # do the evaluations
-
+    print()
+    print("start training".upper().center(40, '='))
+    print(f">>> Train Dataset: {train_data_path}")
+    print(f">>> Eval Dataset: {eval_data_path}")
+    print(f">>> Model: {model_name or model_path}")
+    print(f">>> Will save at: {output_dir}")
+    print()
+    
     # save the results as json file in the following file
     output_path = output_dir / f"eval_{eval_data_path.stem}.json"
     
