bb1d646adfe6e73cdd62b340ec4d6bf48da9375c
Kelian Schekkerman <kelianschekkerman@hotmail.com>
2023-10-29 16:41:30 +0100

    Update in dataset metric to handle new input

--- Code/model/eval.py
+++ Code/model/eval.py
@@ -23,7 +23,17 @@ def eval_predictions(eval_data_path, predictions, labels, output_dir):
     def in_dataset(pred, label):
         # check if prediction is in dataset
         # return tuple: list of bools, none because labels are not used
-        return [any(any(p.lower() == str(item).lower() for item in label) for p in pred_item) for pred_item in pred], None
+        # return [any(any(p.lower() == str(item).lower() for item in label) for p in pred_item) for pred_item in pred], None
+        return [
+            any(
+                any(
+                    isinstance(p, str) and p.lower() == str(item).lower()
+                    for p in pred_sublist
+                )
+                for item in label_sublist
+            )
+            for pred_sublist, label_sublist in zip(pred, label)
+        ], None
 
     conditions = [full_string, first_word, first_three_chars]
 
@@ -34,9 +44,9 @@ def eval_predictions(eval_data_path, predictions, labels, output_dir):
         for k in [1, 3, 5, 10]:
             modified_predictions = [condition(pred, lbl)[0] for pred, lbl in zip(predictions, labels)]
             modified_labels = [condition(pred, lbl)[1] for pred, lbl in zip(predictions, labels)]
+            pred_in_data, _ = in_dataset(modified_predictions, modified_labels)
+            results[condition_name]['percentage_found_in_dataset'] = (sum(pred_in_data) / len(pred_in_data)) * 100
             results[condition_name][f'top_{k}'] = accuracy_at_k(modified_predictions, modified_labels, k)
-        pred_in_data, _ = in_dataset(predictions, labels)
-        results[condition_name]['percentage_found_in_dataset'] = (sum(pred_in_data) / len(pred_in_data)) * 100
 
     # Save results to file
     output_file_path = output_dir/ f"eval_{eval_data_path.stem}.json"
